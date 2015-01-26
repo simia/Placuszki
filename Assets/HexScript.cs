e64dab3cb9837b953929a7aee26ceccb51ac853a
simia <simia.dei@gmail.com>
2015-01-26 18:33:55 +0100

    Initial commit.

---
+++ Assets/HexScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HexScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.name == "Car - Decent Skid") {
+			Application.LoadLevel(Application.loadedLevelName);
+		}
+	}
+}
