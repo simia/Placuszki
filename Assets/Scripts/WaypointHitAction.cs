4e5e0800afb2e5cbe0cf11e033022057512aa959
simia <simia.dei@gmail.com>
2015-02-18 20:19:04 +0100

    changed behavior of way points

--- Assets/Scripts/WaypointHitAction.cs
+++ Assets/Scripts/WaypointHitAction.cs
@@ -6,13 +6,14 @@ public class WaypointHitAction : MonoBehaviour {
 	public int Id = 1;
 	public Material EnteredMatPrefab;
 
-	void OnCollisionEnter(Collision c) {
-		CarController ctrl = c.gameObject.GetComponent<CarController> ();
-
+	void OnTriggerEnter(Collider c) {
+		print ("on triher");
+		CarController ctrl = c.transform.parent.gameObject.GetComponent<CarController> ();
+		
 		if (!ctrl)
 			return;
 
-		if (ctrl.lastWaypointId + 1 == this.Id) {
+		if (ctrl.lastWaypointId + 1 == this.Id) {		
 			renderer.material = EnteredMatPrefab;
 			++ctrl.lastWaypointId;
 		}
